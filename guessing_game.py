62c6deebb6f7d4b936da559c1703b3a63a3ca7c5
Ryan Borden <ryanborden81@gmail.com>
2019-02-15 15:24:44 -0800

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -4,59 +4,70 @@ import sys
 
 all_score = []
 
-def start_game():
-    answer = random.randint(1, 10)
-    attempts = 0
+
+def welcome():
     print("""
     ------------------------------------
     Welcome to the Number Guessing Game!
     See if you can guess the correct
     number and beat the high score!
     ------------------------------------""")
+
+def show_score():
     if len(all_score) >= 1:
         best = high_score(all_score)
         print("The high score is {}!".format(best))
     else:
         print("No high score yet")
+    
+def main():
+    answer = random.randint(1, 10)
+    attempts = 0
+    welcome()
+    show_score()
+    
+    
     while True:
-        user_num = user_input()
-        if user_num < 1 or user_num > 10:
-            print("Please enter a value between 1 & 10")
-        elif user_num == answer:
-            attempts += 1
+        user_num = validate_input()
+        attempts += 1
+        if user_num == answer:
             if attempts == 1:
                 print("Correct! It took {} try!".format(attempts))
             else:
                 print("Correct! It took {} tries!".format(attempts))
             all_score.append(attempts)
-            new_game = input("Would you like to try again? (y)es/(n)o  ")
-            restart(new_game)            
+            break
         elif user_num > answer:
-            attempts += 1
             print("Too high!")
         elif user_num < answer:
-            attempts += 1
             print("Too low!")
+    restart()  
 
-def user_input():
-    try:
-        foo =  int(input("Pick a number between 1 & 10:  "))
-        print("DEBUG!!!!! This the the value of foo ---> {}".format(foo))
-    except ValueError:
-        print("That's not a valid value. Try again")
+def validate_input():
+    while True:
+        try:
+            guess = int(input("Pick a number between 1 & 10:  "))
+        except ValueError:
+            print("That's not a valid value. Try again")
+        else:
+            if guess < 1 or guess > 10:
+                print("Please enter a value between 1 & 10")
+            else:
+                return guess
     
-                
 def high_score(score):
     return min(score)
 
-def restart(response):
-    if response[0].lower() == 'y':
-        start_game()
-    elif response[0].lower() == 'n':
+def restart():
+    new_game = input("Would you like to try again? (y)es/(n)o  ")
+    if new_game[0].lower() == 'y':
+        main()
+    elif new_game[0].lower() == 'n':
         print("Thanks for playing!")
         sys.exit()
     else:
         print("Invalid answer")
         sys.exit()
 
-start_game()
+if __name__ == '__main__':
+    main()
