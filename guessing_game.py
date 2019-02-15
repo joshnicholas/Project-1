4aaf4fa1f03c1c5527eb413ec65b277c3e37e399
Ryan Borden <ryanborden81@gmail.com>
2019-02-15 14:43:26 -0800

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -7,7 +7,6 @@ all_score = []
 def start_game():
     answer = random.randint(1, 10)
     attempts = 0
-    guess = 0
     print("""
     ------------------------------------
     Welcome to the Number Guessing Game!
@@ -20,26 +19,33 @@ def start_game():
     else:
         print("No high score yet")
     while True:
-        try:
-            guess = int(input("Pick a number between 1 & 10:  "))
-        except ValueError:
-            print("That's not a valid value. Try again")
-        else:
-            if guess < 1 or guess > 10:
-                print("Please enter a value between 1 & 10")
-            elif guess == answer:
-                attempts += 1
+        user_num = user_input()
+        if user_num < 1 or user_num > 10:
+            print("Please enter a value between 1 & 10")
+        elif user_num == answer:
+            attempts += 1
+            if attempts == 1:
+                print("Correct! It took {} try!".format(attempts))
+            else:
                 print("Correct! It took {} tries!".format(attempts))
-                all_score.append(attempts)
-                new_game = input("Would you like to try again? (y)es/(n)o  ")
-                restart(new_game)            
-            elif guess > answer:
-                attempts += 1
-                print("Too high!")
-            elif guess < answer:
-                attempts += 1
-                print("Too low!")
+            all_score.append(attempts)
+            new_game = input("Would you like to try again? (y)es/(n)o  ")
+            restart(new_game)            
+        elif user_num > answer:
+            attempts += 1
+            print("Too high!")
+        elif user_num < answer:
+            attempts += 1
+            print("Too low!")
 
+def user_input():
+    try:
+        foo =  int(input("Pick a number between 1 & 10:  "))
+        print("DEBUG!!!!! This the the value of foo ---> {}".format(foo))
+    except ValueError:
+        print("That's not a valid value. Try again")
+    
+                
 def high_score(score):
     return min(score)
 
@@ -47,6 +53,7 @@ def restart(response):
     if response[0].lower() == 'y':
         start_game()
     elif response[0].lower() == 'n':
+        print("Thanks for playing!")
         sys.exit()
     else:
         print("Invalid answer")
